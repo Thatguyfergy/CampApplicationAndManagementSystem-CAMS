51a1b05beba7510e51c4b15614bdf5f8ac78ec9a
Ying Hao <yinghao2151@gmail.com>
2023-10-16 16:14:07 +0800

    CampInfo update

---
+++ cams/CampInfo.java
@@ -0,0 +1,25 @@
+package cams;
+
+public class CampInfo {
+    private String campName;
+    private String dates;
+    private String registrationClosingDate;
+    private String campVisibility;
+    private String location;
+    private String campDescription;
+    private String staffInCharge;
+    private String [] committeeMembers;
+
+    public CampInfo (String campName, String dates, String registrationClosingDate, String campVisibility, String location, String campDescription, String staffInCharge, String [] committeeMembers) {
+        this.campName = campName;
+        this.dates = dates;
+        this.registrationClosingDate = registrationClosingDate;
+        this.campVisibility = campVisibility;
+        this.location = location;
+        this.campDescription = campDescription;
+        this.staffInCharge = staffInCharge;
+        this.committeeMembers = committeeMembers;
+    }
+
+    }
+}
