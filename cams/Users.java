1e85545938369e1f8d26ff6ac25feb5fc74a09a9
thekopiman <thekopiman>
2023-10-16 16:13:33 +0800

    commit abstract users

--- cams/Users.java
+++ cams/Users.java
@@ -1,5 +1,24 @@
 package cams;
 
-public class Users{
+public abstract class Users {
+    private String userID;
+    private String password = "password";
+    private String facultyInfo;
+
+    protected void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public String getID() {
+        return userID;
+    }
+
+    protected Boolean checkPassword(String password) {
+        return password == this.password;
+    }
+
+    public String getFacultyInfo() {
+        return facultyInfo;
+    }
 
 }
