7de720c3f486e3204f595dad548e760eeee90f8f
thekopiman <thekopiman>
2023-11-03 00:14:49 +0800

    Staff

--- CAMSapp.java
+++ CAMSapp.java
@@ -12,20 +12,26 @@ public class CAMSapp {
     public static void main(String[] args) throws IOException {
         // CAMDisplay cams = new CAMDisplay();
         // cams.startScreen();
-        EnquiriesArray enquiriesArray = new EnquiriesArray("csvfiles\\enquiries.csv", "csvfiles\\replies.csv");
+        // EnquiriesArray enquiriesArray = new EnquiriesArray("csvfiles\\enquiries.csv",
+        // "csvfiles\\replies.csv");
 
-        Student yinghaoisgay = new Student("YH", "Gaysch");
+        // Student yinghaoisgay = new Student("YH", "Gaysch");
+        CampArray campArray = new CampArray("csvfiles\\camps.csv");
+
         Staff drloke = new Staff("lokeyman", "gayschool");
 
-        enquiriesArray.submitEnquiry(new Enquiries("Ying hao is gay",
-        yinghaoisgay.getID(), "gay camp"));
+        // enquiriesArray.submitEnquiry(new Enquiries("Ying hao is gay",
+        // yinghaoisgay.getID(), "gay camp"));
 
-        try {
-            enquiriesArray.viewEnquiries(drloke);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        enquiriesArray.replyEnquiry(drloke);
+        // try {
+        // enquiriesArray.viewEnquiries(drloke);
+        // } catch (Exception e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        // enquiriesArray.replyEnquiry(drloke);
+
+        drloke.createCamp(campArray);
+        drloke.viewCamp(campArray);
     }
 }
