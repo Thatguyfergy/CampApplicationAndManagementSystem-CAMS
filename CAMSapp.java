dbdefdad23d6280fd75fb54560756efdb5f0b5d2
thekopiman <thekopiman>
2023-11-02 19:36:50 +0800

    Deleted testing files

--- CAMSapp.java
+++ CAMSapp.java
@@ -1,15 +1,13 @@
 import camps.*;
 import users.*;
 import camsdisplay.*;
-import test.EnquiresNReplies;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class CAMSapp {
     public static void main(String[] args) throws IOException {
-        // CAMDisplay cams = new CAMDisplay();
-        // cams.startScreen();
-        EnquiresNReplies nihao = new EnquiresNReplies();
+        CAMDisplay cams = new CAMDisplay();
+        cams.startScreen();
     }
 }
