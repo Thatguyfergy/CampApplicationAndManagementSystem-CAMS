b726394647ef3789c2464a6effe700799a41a7c7
thekopiman <thekopiman>
2023-11-02 15:25:44 +0800

    Dynamic size of Camp Attendees

--- camps/Camp.java
+++ camps/Camp.java
@@ -7,13 +7,13 @@ import users.Student;
 
 public class Camp {
     private CampInfo campInfo;
-    private String[] attendees;
+    private ArrayList<String> attendees = new ArrayList<String>();
     private String[] committeeMembers;
 
     public Camp() {
         // Create campInfo
         this.campInfo = new CampInfo();
-        this.attendees = new String[10]; // Adjust the size as needed
+        // Adjust the size as needed
         this.committeeMembers = new String[10]; // Initialize committeeMembers array
 
     }
@@ -54,14 +54,17 @@ public class Camp {
         return campInfo.getTotalSlots();
     }
 
-
     public void setCampDescription(String newDescription) {
         campInfo.setCampDescription(newDescription);
     }
 
     // Method to get the list of attendees
     public String[] getAttendees() {
-        return attendees;
+        String[] attend = new String[attendees.size()];
+        for (int i = 0; i < attendees.size(); i++) {
+            attend[i] = attendees.get(i);
+        }
+        return attend;
     }
 
     // Method to get the list of Committee Members
@@ -76,34 +79,34 @@ public class Camp {
         if (currentDate.compareTo(campInfo.getRegistrationClosingDate()) > 0) {
             // Past registration closing date, set visibility to "off"
             return "off";
-        } 
-        else {
-
-        if (occupiedSlots < totalSlots) {
-            // There are available slots, so set visibility to "on"
-            return "on";
         } else {
-            // All slots are taken, set visibility to "off"
-            return "off";
+
+            if (occupiedSlots < totalSlots) {
+                // There are available slots, so set visibility to "on"
+                return "on";
+            } else {
+                // All slots are taken, set visibility to "off"
+                return "off";
+            }
         }
     }
-    }
-    public void registerStudent(Student student, boolean isCampCommittee) {
-    
-    student.registerCamp(getCampName(), isCampCommittee);
 
-    // Update the camp's lists based on the registration type
-    if (isCampCommittee) {
-        // Register as a camp committee member
-        String[] committeeMembers = getCommitteeMembers();
-        // Add the student to the committeeMembers array
-        committeeMembers[committeeMembers.length] = student.getID();
-    } else {
-        // Register as an attendee
-        String[] attendees = getAttendees();
-        // Add the student to the attendees array
-        attendees[attendees.length] = student.getID();
+    public void registerStudent(Student student, boolean isCampCommittee) {
+
+        student.registerCamp(getCampName(), isCampCommittee);
+
+        // Update the camp's lists based on the registration type
+        if (isCampCommittee) {
+            // Register as a camp committee member
+            String[] committeeMembers = getCommitteeMembers();
+            // Add the student to the committeeMembers array
+            committeeMembers[committeeMembers.length] = student.getID();
+        } else {
+            // Register as an attendee
+            String[] attendees = getAttendees();
+            // Add the student to the attendees array
+            attendees[attendees.length] = student.getID();
+        }
     }
-}
 
 }
