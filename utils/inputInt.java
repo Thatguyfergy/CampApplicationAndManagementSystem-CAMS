6f079b29cfdb6003db5b95f74a3db0608653e604
thekopiman <thekopiman>
2023-11-07 17:05:13 +0800

    Fixed invalid input problem

---
+++ utils/inputInt.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class inputInt {
+
+    public static int nextInt(Scanner scanner) {
+        int value;
+        while (true) {
+            try {
+                value = scanner.nextInt();
+                return value;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input.");
+                System.out.println("Input again:");
+            }
+        }
+    }
+}
