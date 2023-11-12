7b60de026fad59f7ed8cfce7910029fc87bacbef
Ying Hao <yinghao2151@gmail.com>
2023-11-12 17:11:06 +0800

    Cleaned up CAMSapp code

--- CAMSapp.java
+++ CAMSapp.java
@@ -1,42 +1,12 @@
-import camps.*;
-import users.*;
 import camsdisplay.*;
-import infoexchange.Enquiries;
-import infoexchange.EnquiriesArray;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class CAMSapp {
     public static void main(String[] args) throws IOException {
-        // CAMDisplay cams = new CAMDisplay();
-        // cams.startScreen();
-        // EnquiriesArray enquiriesArray = new EnquiriesArray("csvfiles\\enquiries.csv",
-        // "csvfiles\\replies.csv");
-
-        // Student yinghaoisgay = new Student("YH", "Gaysch");
-        // CampArray campArray = new CampArray("csvfiles\\camps.csv");
-
-        // Staff drloke = new Staff("lokelokeloke", "lokeyman", "gayschool");
-
-        // enquiriesArray.submitEnquiry(new Enquiries("Ying hao is gay",
-        // yinghaoisgay.getID(), "gay camp"));
-
-        // try {
-        // enquiriesArray.viewEnquiries(drloke);
-        // } catch (Exception e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-        // enquiriesArray.replyEnquiry(drloke);
-
-        // drloke.createCamp(campArray);
-        // drloke.viewCamp(campArray);
-
         CAMDisplay cams = new CAMDisplay();
         while (true) {
             cams.startScreen();
-            // cams.userScreen(new Staff("Loke", "LYW023", "SCSE"));
         }
 
     }
