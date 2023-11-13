26271f80681d5dc8c573f74bc003098f4423d7e9
thekopiman <thekopiman>
2023-11-13 20:42:21 +0800

    CAMSapp committed

--- CAMSapp.java
+++ CAMSapp.java
@@ -1,12 +1,35 @@
 import camsdisplay.*;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class CAMSapp {
     public static void main(String[] args) throws IOException {
+
+        // Now there are 2 implementations, DisplayLogin that use DIP and Noob
+        // CAMDisplay
+        // Comment out the one you dw to use, tyvm
+
         // DisplayLogin cams = new DisplayLogin(true);
+
         CAMDisplay cams = new CAMDisplay();
+
+        if (cams instanceof CAMDisplay) {
+            System.out.println("Currently running CampDisplay!");
+        } else {
+            System.out.println("Currently running DisplayLogin!");
+        }
+
+        System.out.println("This shit is running on CAMSapp.java, go and dig for this file.");
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         while (true) {
+
             cams.startScreen();
         }
 
