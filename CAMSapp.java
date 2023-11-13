a37c1931351e3469ecbbd9ca779ae13857b81d0b
thekopiman <thekopiman>
2023-11-13 20:24:46 +0800

    yh lame

--- CAMSapp.java
+++ CAMSapp.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class CAMSapp {
     public static void main(String[] args) throws IOException {
-        DisplayLogin cams = new DisplayLogin(true);
-        // CAMDisplay cams = new CAMDisplay();
+        // DisplayLogin cams = new DisplayLogin(true);
+        CAMDisplay cams = new CAMDisplay();
         while (true) {
             cams.startScreen();
         }
