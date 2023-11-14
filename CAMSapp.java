c273a3c738eeaf6f4b3e59a93c5aa4c48f8ba714
thekopiman <thekopiman>
2023-11-14 16:20:38 +0800

    Removed Old implementations of CAMDisplay

--- CAMSapp.java
+++ CAMSapp.java
@@ -6,28 +6,8 @@ import java.util.concurrent.TimeUnit;
 public class CAMSapp {
     public static void main(String[] args) throws IOException {
 
-        // Now there are 2 implementations, DisplayLogin that use DIP and Noob
-        // CAMDisplay
-        // Comment out the one you dw to use, tyvm
-
         DisplayLogin cams = new DisplayLogin(true);
 
-        // CAMDisplay cams = new CAMDisplay();
-
-        // if (cams instanceof CAMDisplay) {
-        //     System.out.println("Currently running CampDisplay!");
-        // } else {
-        //     System.out.println("Currently running DisplayLogin!");
-        // }
-
-        System.out.println("This shit is running on CAMSapp.java, go and dig for this file.");
-
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
         while (true) {
 
             cams.startScreen();
