c5dcc500af0e4e0938b526a3d8f25f9f8668dfa5
Ying Hao <yinghao2151@gmail.com>
2023-11-17 15:00:04 +0800

    comments for CampFilter

--- camps/CampFilter.java
+++ camps/CampFilter.java
@@ -18,14 +18,8 @@ public class CampFilter {
     private Scanner sc;
 
     // The `public CampFilter()` is a constructor method for the `CampFilter` class.
-    // It initializes the
-    // instance variables `availableDates`, `availableFaculties`,
-    // `availableLocations`, `availableSIC`,
-    // `filteredCamps`, and `unfilteredCamps` as empty ArrayLists. It also
-    // initializes the `sc`
-    // variable as a new instance of the `Scanner` class, which reads input from the
-    // standard input
-    // stream.
+    // It initialises 'availableDates', 'availableFaculties', 'availableLocations',
+    // 'availableSIC', 'filteredCamps', 'unfilteredCamps', and 'sc' variables.
     public CampFilter() {
         availableDates = new ArrayList<String>();
         availableFaculties = new ArrayList<String>();
@@ -37,16 +31,14 @@ public class CampFilter {
     }
 
     /**
-     * The function adds a date to a list of available dates if it is not already
-     * present.
+     * The method 'addAvailableDate()' adds a date to 'availableDates' ArrayList
+     * if it is not already in the list
      * 
-     * @param date The parameter "date" is a String representing a date that you
-     *             want to add to the
-     *             list of available dates.
-     * @return The method is returning a boolean value. It returns true if the date
-     *         is successfully
-     *         added to the list of available dates, and false if the date is
-     *         already in the list.
+     * @param date The parameter "date" is a String that represents a date 
+     *             to be added to 'availableDates' ArrayList
+     * @return The method returns a boolean value. 
+     *         It returns true if the date is successfully added to the list,
+     *         and false if the date is already in the list.
      */
     public boolean addAvailableDate(String date) {
         if (!availableDates.contains(date)) {
@@ -56,16 +48,14 @@ public class CampFilter {
     }
 
     /**
-     * The function adds a faculty to a list of available faculties if it is not
-     * already in the list.
+     * The method 'addAvailableFaculty()' adds a faculty to 'availableFaculties' ArrayList
+     * if it is not already in the list
      * 
-     * @param faculty The parameter "faculty" is a String representing the name of a
-     *                faculty that you
-     *                want to add to the list of available faculties.
-     * @return The method is returning a boolean value. It returns true if the
-     *         faculty is successfully
-     *         added to the list of available faculties, and false if the faculty is
-     *         already in the list.
+     * @param faculty The parameter "faculty" is a String that represents
+     *                the faculty that to be added to 'availableFaculties' ArrayList
+     * @return The method returns a boolean value. 
+     *         It returns true if the faculty is successfully added to the list,
+     *         and false if the faculty is already in the list.
      */
     public boolean addAvailableFaculty(String faculty) {
         if (!availableFaculties.contains(faculty)) {
@@ -75,17 +65,14 @@ public class CampFilter {
     }
 
     /**
-     * The function adds a location to a list of available locations if it is not
-     * already in the list.
+     * The method 'addAvailableLocation()' adds a location to 'availableLocations' ArrayList
+     * if it is not already in the list
      * 
      * @param location The location parameter is a String that represents the
-     *                 location to be added to
-     *                 the list of available locations.
-     * @return The method is returning a boolean value. It returns true if the
-     *         location was
-     *         successfully added to the availableLocations list, and false if the
-     *         location is already in the
-     *         list.
+     *                 location to be added to 'availableLocations' ArrayList
+     * @return The method returnds a boolean value. 
+     *         It returns true if the location was successfully added to the list,
+     *         and false if the location is already in the list.
      */
     public boolean addAvailableLocation(String location) {
         if (!availableLocations.contains(location)) {
@@ -95,16 +82,14 @@ public class CampFilter {
     }
 
     /**
-     * The function addAvailableSIC checks if a given Staff In Charge is already in
-     * the availableSIC list and adds
-     * it if it is not.
+     * The method 'addAvailableSIC()' adds a staff in charge to 'availableSIC' ArrayList
+     * if it is not already in the list
      * 
-     * @param sic The parameter "sic" is a String representing a Staff In Charge
-     * @return The method is returning a boolean value. It returns true if the given
-     *         sic is not already
-     *         in the availableSIC list and it is successfully added to the list. It
-     *         returns false if the given
-     *         sic is already in the list and cannot be added again.
+     * @param sic The parameter "sic" is a String representing a Staff In Charge 
+     *            to be added to 'availableSIC' ArrayList
+     * @return The method returns a boolean value. 
+     *         It returns true if sic was successfully added to the list,
+     *         and false if sic is already in the list.
      */
     public boolean addAvailableSIC(String sic) {
         if (!availableSIC.contains(sic)) {
@@ -114,12 +99,12 @@ public class CampFilter {
     }
 
     /**
-     * The function filters a list of camps based on user input for various criteria
-     * such as date,
-     * faculty, location, and staff in charge.
+     * The method 'filter(Users user)' filters a list of camps based on user's input for the type of filter,
+     * such as date, faculty, location, and staff in charge.
+     * if the user enters an invalid choice, no filter will be applied.
      * 
      * @param user The "user" parameter is an instance of the "Users" class.
-     * @return The method is returning an ArrayList of Camp objects.
+     * @return The method returns the ArrayList<Camp> of filtered camps.
      */
     public ArrayList<Camp> filter(Users user) {
         System.out.println("Filter by: ");
@@ -158,9 +143,11 @@ public class CampFilter {
     }
 
     /**
-     * The function filterByDate() prompts the user to enter a date in the format
-     * dd/mm/yyyy, validates
-     * the input, and filters a list of camps based on the entered date.
+     * The method 'filterByDate()' filters a list of camps by a user-selected date.
+     * The user is prompted to enter a date in the format dd/mm/yyyy
+     * if user enters an invalid date, user will be prompted to enter date again.
+     * using the given date, the method adds all camps that have the given date
+     * into the 'filteredCamps' ArrayList.
      */
     private void filterByDate() {
         String date;
@@ -183,11 +170,14 @@ public class CampFilter {
     }
 
     /**
-     * The function filters a list of camps based on the faculty of a user.
+     * The method 'filterByFaculty(Users user)' filters a list of camps by a user-selected faculty.
+     * If user is a Student, the method will display the student's faculty and NTU as options.
+     * If user is a Staff, the method will display all available faculties as options.
+     * The user is prompted to enter a faculty to filter by.
+     * The method adds all camps that have the given faculty into the 'filteredCamps' ArrayList.
      * 
      * @param user The user parameter is an instance of the Users class, which is
-     *             the parent class for
-     *             both the Student and other user types.
+     *             the parent class for both the Student and Staff classes.
      */
     private void filterByFaculty(Users user) {
         String faculty;
@@ -238,7 +228,9 @@ public class CampFilter {
     }
 
     /**
-     * The function filters a list of camps by a user-selected location.
+     * The method 'filterByLocation()' filters a list of camps by a user-selected location.
+     * The user is prompted to enter a location to filter by.
+     * The method adds all camps that have the given location into the 'filteredCamps' ArrayList.
      */
     private void filterByLocation() {
         String location;
@@ -265,7 +257,9 @@ public class CampFilter {
     }
 
     /**
-     * The function filters a list of camps by a specified staff in charge (SIC).
+     * The method 'filterBySIC()' filters a list of camps by a user-selected staff in charge.
+     * The user is prompted to enter a staff in charge to filter by.
+     * The method adds all camps that have the given staff in charge into the 'filteredCamps' ArrayList.
      */
     private void filterBySIC() {
 
@@ -293,9 +287,8 @@ public class CampFilter {
     }
 
     /**
-     * The function sets the value of the unfilteredCamps variable to the provided
-     * ArrayList of Camp
-     * objects.
+     * The method 'setUnfilterCamps' sets the 'unfilteredCamps' ArrayList to the
+     * given ArrayList of Camp objects.
      * 
      * @param camps The "camps" parameter is an ArrayList of objects of type Camp.
      */
@@ -304,7 +297,7 @@ public class CampFilter {
     }
 
     /**
-     * The function clears the contents of the "filteredCamps" list.
+     * The method 'clearFilteredCamps()' clears the 'filteredCamps' ArrayList.
      */
     public void clearFilteredCamps() {
         filteredCamps.clear();
